365c25e33ccdd1a0b688c6ef43a9698eb14ccd8d
TanveetGill <tanveet.gill@gmail.com>
2016-07-10 15:38:59 -0400

    Added clickable buttons feature

--- fb_onboardingbot/views.py
+++ fb_onboardingbot/views.py
@@ -5,6 +5,17 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json
 import requests
+import re
+import random
+
+jokes = {
+         'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
+                    """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
+         'fat':    ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""",
+                    """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """],
+         'dumb':   ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""",
+                    """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""]
+         }
 
 # Create your views here.
 class OnBoardingBotView(generic.View):
@@ -33,8 +44,40 @@ class OnBoardingBotView(generic.View):
                     post_facebook_message(message['sender']['id'], message['message']['text'])   
         return HttpResponse()
 
-def post_facebook_message(fbid, recevied_message):           
+def post_facebook_message(fbid, recevied_message):
+    # Remove all punctuations, lower case the text and split it based on space
+    tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
+    start_text = ''
+    for token in tokens:
+        if token in jokes:
+            start_text = random.choice(jokes[token])
+            break
+    if not start_text:
+        start_text = "Hey, congrats on getting the job! Im Sarah and I can help you get settled into your new role! We can get started right now with the HR procedure or you can see other options:"
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHePhy0EvMBAKdbSfQnZAG5QuZBrAZCZA0lGZBVmJrWtg2yxxoQNfvHZAAZAidNdIi2FEJjfiLeO3TZAZC8pvVKykhDwDHP14zQ1cW384kKGR3578fzn8Firby89ZBit6ITmZBOwu0VSjWpZBzDfZBItJ4P4fsUy6PCuybrNHoXcymzHbgZDZD' 
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
+    response_msg = json.dumps({
+        "recipient":{"id":fbid},
+        "message": {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":start_text,
+                "buttons":[
+                  {
+                    "type":"postback",
+                    "title":"Get Started",
+                    "payload":";klf;kkkl;"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Options",
+                    "payload":"USER_DEFINED_PAYLOAD"
+                  }
+                ]
+              }
+            } 
+          }
+        })
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     print(status.json())
